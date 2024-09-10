be939b3faf009d2f5161bdc508fdca312e873596
ThanhSang2107 <lethanhsang2107@gmail.com>
2024-09-10 15:37:51 +0700

    gui thay

---
+++ BT02/BT02/Controllers/Theloaicontroller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BT02.Controllers
+{
+    public class TheLoaiController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
