b037a77c4e74fa12067727aca0029da0a8536a76
ThanhSang2107 <lethanhsang2107@gmail.com>
2024-09-17 00:08:40 +0700

    em gui thay

---
+++ Baitapkiemtra/Baitapkiemtra/Controllers/TaiKhoanController.cs
@@ -0,0 +1,23 @@
+﻿using Baitapkiemtra.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Baitapkiemtra.Controllers
+{
+    public class TaiKhoanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult DangKy(TaiKhoanViewModel taikhoan)
+        {
+
+            if (taikhoan != null && taikhoan.Password != null && (taikhoan.Password).Length > 0 && taikhoan.Fullname != null && taikhoan.Age > 0)
+            {
+                taikhoan.Password = taikhoan.Password + "_chuoi_ma_hoa";
+            }
+
+            return View(taikhoan);
+        }
+    }
+}
---
+++ Baitapkiemtra/Baitapkiemtra/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Baitapkiemtra.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
